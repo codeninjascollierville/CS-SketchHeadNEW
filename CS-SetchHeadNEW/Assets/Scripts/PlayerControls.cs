b35c2f0a2f008bdcaf471e8373e235d9d96e3960
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-09-20 19:01:22 -0500

    not done
    not done

---
+++ CS-SetchHeadNEW/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Rigidbody")]
+    public Rigibody2D rb;
+
+    [Header("Default Down Speed")]
+    public float DownSpeed = 20f;
+
+    [Header("Default Movement Speed ")]
+    public float MovmentSpeed = 10f;
+
+    [Header("Rigidbody")]
+    public Rigibody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
