2d5683c54134708bc9ea7a13ff2e4b8d350449ca
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-09-29 18:41:25 -0500

    number 17
    not done

--- CS-SetchHeadNEW/Assets/Scripts/PlayerControls.cs
+++ CS-SetchHeadNEW/Assets/Scripts/PlayerControls.cs
@@ -7,24 +7,32 @@ public class PlayerControls : MonoBehaviour
 {
     [Header("Rigidbody")]
     public Rigibody2D rb;
-
     [Header("Default Down Speed")]
+
+
     public float DownSpeed = 20f;
-
     [Header("Default Movement Speed ")]
-    public float MovmentSpeed = 10f;
 
-    [Header("Rigidbody")]
-    public Rigibody2D rb;
+
+    public float MovmentSpeed = 10f;
+    [Header("Default Directional Movement Speed")]
+    public float movement = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigibody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        if (movement < 0)
+        {
+            this.GetComponent<SpiritRender>().flipX = false;
+        }
 
     }
 }
