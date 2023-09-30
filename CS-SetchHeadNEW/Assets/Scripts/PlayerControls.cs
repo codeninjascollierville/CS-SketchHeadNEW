2e1bdcde3a76cec63983e90e0735ea21a28d49c2
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-09-30 11:24:35 -0500

    number 20
    not done

--- CS-SetchHeadNEW/Assets/Scripts/PlayerControls.cs
+++ CS-SetchHeadNEW/Assets/Scripts/PlayerControls.cs
@@ -6,33 +6,84 @@ using UnityEngine.UI;
 public class PlayerControls : MonoBehaviour
 {
     [Header("Rigidbody")]
-    public Rigibody2D rb;
+    public Rigidbody2D rb;
     [Header("Default Down Speed")]
 
 
-    public float DownSpeed = 20f;
+    public float downSpeed = 20f;
     [Header("Default Movement Speed ")]
 
 
-    public float MovmentSpeed = 10f;
+    public float movementSpeed = 10f;
     [Header("Default Directional Movement Speed")]
+
     public float movement = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigibody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
         movement = Input.GetAxis("Horizontal") * movementSpeed;
-
+        //d
         if (movement < 0)
         {
-            this.GetComponent<SpiritRender>().flipX = false;
+
+            this.GetComponent<SpriteRenderer>().flipX = false;
         }
+
+        else
+        {
+
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
 
     }
 }
+
+
+
+
+
+
+
+void FixedUpdate()
+{
+
+
+ Vector2 velocity = rb.Velocity;
+ 
+
+
+velocity.x = movement;
+
+rb.velocity = velocity;
+
+
+
+}
+
+
+
+
+
+private void OnCollisionEnter2D(Collision2D Collision2D collision)
+{
+
+    
+    rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
+}
+
+
+
+
+
+
+
